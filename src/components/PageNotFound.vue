3ad821f2e54de2c143b4c86b8d53f09af94099bd
cSDes1gn <christian911@sympatico.ca>
2021-03-27 23:38:29 -0400

    404 not found page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="col-full text-center">
+    <h1>404 Page Not Found</h1>
+    <router-link :to="{name: 'Home'}">Read threads here</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
