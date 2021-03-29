525ab195be6f96a34887749644e9475755d359a5
cSDes1gn <christian911@sympatico.ca>
2021-03-28 22:16:01 -0400

    user card edit and mutations from store

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,6 +3,7 @@
     <div class="flex-grid">
       <div class="col-3 push-top">
         <user-profile-card :user="user"/>
+        <user-profile-card-editor :user="user"/>
         <p class="text-xsmall text-faded text-center">Member since june 2003, last visited 4 hours ago</p>
         <div class="text-center">
           <hr>
@@ -26,12 +27,14 @@
 <script>
 import UserProfileCard from '@/components/UserProfileCard'
 import PostList from '@/components/PostList'
+import UserProfileCardEditor from '@/components/UserProfileCardEditor'
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
     PostList,
-    UserProfileCard
+    UserProfileCard,
+    UserProfileCardEditor
   },
   computed: {
     ...mapGetters({ user: 'authUser' })
